4ce35db64841ebdd9cfbb4b6644268061fe3ae1f
Cheren <kalistratov.s.m@gmail.com>
2022-01-30 00:40:48 +0400

    Add request in Ship/Parents/Requests

--- app/Ship/Parents/Requests/Request.php
+++ app/Ship/Parents/Requests/Request.php
@@ -4,7 +4,13 @@ namespace App\Ship\Parents\Requests;
 
 use Apiato\Core\Abstracts\Requests\Request as AbstractRequest;
 
+/**
+ * Class Request
+ *
+ * @property-read   string id
+ *
+ * @package App\Ship\Parents\Requests
+ */
 abstract class Request extends AbstractRequest
 {
-
 }
