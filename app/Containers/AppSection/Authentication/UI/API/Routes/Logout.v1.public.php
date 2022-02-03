1ff043c7599fa69818be4c77d831ea8fbb9c92d8
Cheren <kalistratov.s.m@gmail.com>
2022-02-03 15:02:46 +0400

    Add php head anatation for app section authentication

--- app/Containers/AppSection/Authentication/UI/API/Routes/Logout.v1.public.php
+++ app/Containers/AppSection/Authentication/UI/API/Routes/Logout.v1.public.php
@@ -1,6 +1,16 @@
 <?php
 
 /**
+ * Beauty application system
+ *
+ * This file is part of the Beauty application system package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    Proprietary
+ * @copyright  Copyright (C) kalistratov.ru, All rights reserved.
+ * @link       https://kalistratov.ru
+ *
  * @apiGroup           OAuth2
  * @apiName            Logout
  * @api                {DELETE} /v1/logout Logout
